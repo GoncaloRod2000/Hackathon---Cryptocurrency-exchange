6efcf2a21b56c54477bbe8071f0f683116efc8ea
GoncaloRodrigues <goncasrod2000@gmail.com>
2023-06-29 12:34:23 +0200

    app.jsx

--- resources/js/Trade/App.jsx
+++ resources/js/Trade/App.jsx
@@ -1,9 +1,13 @@
 import TradeData from "./TradeData";
+import { DateTime } from "luxon";
+import PriceChart from "./PriceChart"
 
 export default function App(){
 
+    const time = (new DateTime(1364774820)).toFormat('yyyy LL dd, HH:mm')
 
     return (
-        <TradeData />
+        <p>The time is {time}</p>
+        // <TradeData />
     )
 }
