0f48cb28efde9c95cd9a37a6e420cc3ef93d0e55
GoncaloRodrigues <goncasrod2000@gmail.com>
2023-06-29 14:42:10 +0200

    timer

--- resources/js/Trade/App.jsx
+++ resources/js/Trade/App.jsx
@@ -1,13 +1,39 @@
+import { useState, useEffect } from "react";
 import TradeData from "./TradeData";
 import { DateTime } from "luxon";
-import PriceChart from "./PriceChart"
+
 
 export default function App(){
 
-    const time = (new DateTime(1364774820)).toFormat('yyyy LL dd, HH:mm')
+    // const time = (new DateTime(1364774820)).toFormat('yyyy LL dd, HH:mm')
+
+    const [time, setTime] = useState(0);
+
+    const increaseTime = () => {
+        setTime(time + 60)
+    }
+
+    useEffect(() => {
+        // creating a timeout when the component mounts, so...
+        const myTimeout = setTimeout(() => {
+            increaseTime()
+        }, 1000);
+     
+        // ...returning a cleanup function which will be run
+        // when the component unmounts
+        return () => {
+            clearTimeout(myTimeout);
+        }
+    }, [time])
 
     return (
-        <p>The time is {time}</p>
-        // <TradeData />
+        <>
+            <p>The time is { DateTime.fromSeconds(1364774820 + time).toFormat('yyyy LL dd, HH:mm')}</p>
+            {/* <button onClick={() => {setClicks(clicks+1)}}>{clicks}</button> */}
+        </>
     )
-}
+
+}   
+
+
+
